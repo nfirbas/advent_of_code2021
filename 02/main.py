4d82bd4acd62bf5e31709794d826ea8bcc1e6df1
nfirbas <nejc.firbas@gmail.com>
2021-12-02 23:39:30 +0100

    day_02

---
+++ 02/main.py
@@ -0,0 +1,21 @@
+def solve(f):
+    forward = 0
+    depth = 0
+    aim = 0
+    for x in f:
+        change = int(x.split(" ")[1])
+        match x.split(" ")[0]:
+            case "forward":
+                forward += change
+                depth += change*aim
+            case "down":
+                aim += change
+            case "up":
+                aim -= change
+    print(aim * forward)
+    print(depth * forward)
+
+if __name__ == "__main__":
+    f = open("./input.txt", "r")
+    solve(f)
+    f.close()
